b9ae259bca9ed95f9ca516c7f89f27ba34d5fe26
mervebingool <mervebingool@gmail.com>
2021-02-03 01:10:21 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryDemo
+{
+    class MyDictionary<S, Y> // Generic Class
+    {
+
+        S[] _array;
+        S[] _tempArray;
+
+        Y[] array;
+        Y[] tempArray;
+
+
+
+        public MyDictionary()
+        {
+            _array = new S[0];
+            array = new Y[0];
+        }
+
+        public void Add(S sehir, Y yemek)
+        {
+
+            _tempArray = _array;
+            tempArray = array;
+
+            _array = new S[_array.Length + 1];
+            array = new Y[array.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+                array[i] = tempArray[i];
+            }
+
+            _array[_array.Length - 1] = sehir;
+            array[array.Length - 1] = yemek;
+
+
+
+        }
+        public void Write()
+        {
+
+            for (int i = 0; i < _array.Length; i++)
+            {
+                Console.WriteLine(_array[i] + " ---> " + array[i]);
+            }
+
+        }
+    }
+}
+
+    
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, string> sehirVeYemek = new MyDictionary<int, string>();
+            sehirVeYemek.Add(25," Cağ Kebabı");
+            sehirVeYemek.Add(16," İskender");
+            sehirVeYemek.Add(33," Tantuni");
+            sehirVeYemek.Add(01,"  Adana Kebap");
+            sehirVeYemek.Add(61," Mıhlama");
+
+            sehirVeYemek.Write();
+            Console.ReadKey();
+
+
+
+        }
+    }
+
+}
+
+
+
+    
+
+
+
+
